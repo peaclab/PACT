7d441ddde009e2e7e19c717d4d1ed36fefab99e2
Zihao Yuan <yuan1z@bu.edu>
2020-05-08 14:34:48 -0400

    latest version of SPICE

--- src/SPICESolver_transient.py
+++ src/SPICESolver_transient.py
@@ -182,7 +182,7 @@ class SPICE_transientSolver:
 		    #Zihao: I don't know why both layer1 and layer2 has power in this case, the ptrace and flp shows only the first layers has power. I need to ask prachi about this self.I.items.
                     #if layer == 0:
                     if layer!= self.layer_limit and self.I[layer][row][col]!=0:
-                        myfile.write("I_{}_{}_{} GND Node{}_{}_{} {}A\n".format(layer,row,col,layer, row, col, self.I[layer][row][col]))
+                        myfile.write("I_{}_{}_{} GND Node{}_{}_{} PULSE(0 {}A 0s 0s 0s 33.3ms 33.3ms)\n".format(layer,row,col,layer, row, col, self.I[layer][row][col]))
                 #east resistance
                     if col != self.col_limit:
                         myfile.write("R_{}_{}_{}_1 Node{}_{}_{} Node{}_{}_{} {}\n".format(layer,row,col,layer, row, col,layer,row,col+1,Re))
