9e5b9b9301267a3022c174a8910807986b086070
Zihao Yuan <yuan1z@bu.edu>
2020-05-29 11:31:03 -0400

    solve the transient block temperature bug

--- src/SPICESolver_transient.py
+++ src/SPICESolver_transient.py
@@ -208,7 +208,7 @@ class SPICE_transientSolver:
                 myfile.write(".SAVE TYPE=IC\n")
                 myfile.write(".end\n")
         os.system("Xyce RC_transient.cir")
-      #  with open('RC_transient_prachi.cir.csv','r') as myfile:
-      #      tmp = np.asarray(list(map(float,list(myfile)[1][:].split(','))))
-      #      reshape_x = tmp.reshape(self.nl,self.nr,self.nc)
-      #  return reshape_x
+        with open('RC_transient.cir.csv','r') as myfile:
+            tmp = np.asarray(list(map(float,list(myfile)[-1][:].split(',')[1:])))
+            reshape_x = tmp.reshape(self.nl,self.nr,self.nc)
+        return reshape_x
