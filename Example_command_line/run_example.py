935383aa9d52270a4acfaeeaba25ed5712811072
Prachi Shukla <prachis@bu.edu>
2020-07-04 19:21:42 -0400

    New files for multiple ptrace run in Example_command_line dir

---
+++ Example_command_line/run_example.py
@@ -0,0 +1,5 @@
+import os
+
+#os.system("python ../src/PACT.py example_lcf_2ptraces.csv example.config example_modelParams_SuperLU.config --gridSteadyFile example.grid.steady")
+os.system("python ../src/PACT.py example_lcf_2ptraces.csv example.config  example_modelParams.config --gridSteadyFile example.grid.steady") # For Zihao
+#os.system("python ../src/PACT.py example_lcf.csv example.config example_modelParams_SuperLU.config --gridSteadyFile example.grid.steady")
