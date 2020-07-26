0e6df47b088909b21db5af9ccf0cd693c55b5429
sofiane chetoui <schetoui@sofianes-MacBook-Pro.local>
2020-07-26 14:02:53 -0400

    OpenRoad commit

--- src/OpenRoad/gridmesh.py
+++ src/OpenRoad/gridmesh.py
@@ -1,72 +1,110 @@
+"""usage: gridmesh.py [-h] --deff  --gridsize
+
+Generates power map
+
+optional arguments:
+  -h, --help   show this help message and exit
+  --deff       Path to the routed def file
+  --gridsize   gridsize"""
+
 import argparse 
-
-parser= argparse.ArgumentParser(description= 'Generates power map')
-parser.add_argument('--deff',type=str,required=True, metavar='',help='Path to the routed def file')
-parser.add_argument('--gridsize',type=int,required=True, metavar='',help='gridsize')
-args= parser.parse_args()
+import sys
+import os.path
 
 
 
+def read_file(filepath):
+	try:
+		if(os.path.exists(filepath)==False):
+			print("Error ! the following file does not exist : ",filepath)
+			exit()
+		with open(filepath, 'r') as f:
+			return f.readlines()
+	except IOError:
+		 print('Could not read the following file:',filepath)
+	
+		
+def write_file(filepath,line):
+	try:		
+		with open(filepath, 'a') as f:
+			f.write(line)
+		f.close()
+	except IOError:
+		print('Could not write in the following file:',filepath)
+
+
+# Parses the arguments from the command line
+def ParseArg():
+	parser= argparse.ArgumentParser(description= 'Generates power map')
+	parser.add_argument('--deff',type=str,required=True, metavar='',help='Path to the routed def file')
+	parser.add_argument('--gridsize',type=int,required=True, metavar='',help='gridsize')
+	return parser.parse_args()
 
 
 
-fp=open(args.deff)
-gridsize=args.gridsize
-power_blocks=[0.0]*gridsize*gridsize
-xcoord_blocks=[]
-ycoord_blocks=[]
-mylines=[]
+# Parses the design dimensions from the def file
+def DesignDim(args):	
+	mylines=[]
+	
+	listline=read_file(args.deff)
+
+	for line in listline:
+		mylines.append(line.split())
+
+	for i in range(len(mylines)):
+		if(len(mylines[i])!=0):
+			if(mylines[i][0]=='DIEAREA'):
+				break
+
+	xdim=int(mylines[i][6])
+	ydim=int(mylines[i][7])
+	return xdim,ydim;
 
 
-listline=fp.readlines()
-mylines=[]
+# Generates the grid " flp" contains the coordinates and dimensions and "ptrace" contains the power values
+def GenerateGrid(args,xdim,ydim):
+	mylines=[]
+	power_blocks=[0.0]*args.gridsize*args.gridsize
+	xcoord_blocks=[]
+	ycoord_blocks=[]
+	listlines=read_file('outputhotspot')
+	
+	for line in listlines:
+		mylines.append(line.split())
+		
+	for i in range(len(mylines)):
+		mylines[i][0]=float(mylines[i][0])
+		mylines[i][1]=float(mylines[i][1])
+		mylines[i][2]=float(mylines[i][2])
 
-for line in listline:
-        mylines.append(line.split())
+	flags=[1]*len(mylines)
+	block_index=-1
+	
+	for i in range(0,xdim-1,int(xdim/args.gridsize)):
+		for j in range(0,ydim-1,int(ydim/args.gridsize)):
+			xcoord_blocks.append(i)
+			ycoord_blocks.append(j)
+			block_index=block_index+1
+			for z in range(len(mylines)):
+				if(flags[z]):
+					if(i<=mylines[z][1]<i+xdim/args.gridsize and j<=mylines[z][2]<j+ydim/args.gridsize):
+						flags[z]=0
+						power_blocks[block_index]=power_blocks[block_index]+mylines[z][0]
 
-for i in range(len(mylines)):
-        if(len(mylines[i])!=0):
-                if(mylines[i][0]=='DIEAREA'):
-                        break
+	for i in range(len(power_blocks)):
+		st='%s %s %s %s\n'%(int(xdim/args.gridsize),int(ydim/args.gridsize),xcoord_blocks[i],ycoord_blocks[i])
+		write_file('flp',st)
 
-xdim=int(mylines[i][6])
-ydim=int(mylines[i][7])
-
-fp=open('./outputhotspot')
-listlines=fp.readlines()
-
-for line in listlines:
-	mylines.append(line.split())
-
-for i in range(len(mylines)):
-	mylines[i][0]=float(mylines[i][0])
-	mylines[i][1]=float(mylines[i][1])
-	mylines[i][2]=float(mylines[i][2])
-
-flags=[1]*len(mylines)
-block_index=-1
-
-for i in range(0,xdim-1,int(xdim/gridsize)):
-        for j in range(0,ydim-1,int(ydim/gridsize)):
-                xcoord_blocks.append(i)
-                ycoord_blocks.append(j)
-                block_index=block_index+1
-                for z in range(len(mylines)):
-                        if(flags[z]):
-                                if(i<=mylines[z][1]<i+xdim/gridsize and j<=mylines[z][2]<j+ydim/gridsize):
-                                        flags[z]=0
-                                        power_blocks[block_index]=power_blocks[block_index]+mylines[z][0]
+	for i in range(len(power_blocks)):
+		st='%s '%(power_blocks[i])
+		write_file('ptrace',st)
 
 
-for i in range(len(power_blocks)):
-        st='%s %s %s %s\n'%(int(xdim/gridsize),int(ydim/gridsize),xcoord_blocks[i],ycoord_blocks[i])
-        with open("flp", "a") as f:
-                f.write(st)
 
-for i in range(len(power_blocks)):
-        st='%s '%(power_blocks[i])
-        with open("ptrace", "a") as f:
-                f.write(st)
-
+if __name__ == "__main__":
+	args= ParseArg()
+	xdim,ydim= DesignDim(args)
+	GenerateGrid(args,xdim,ydim)
+	
 
 
---
+++ src/OpenRoad/opensta_parser.py
@@ -0,0 +1,155 @@
+"""
+
+Generates power map
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --lib       Path to the library file
+  --lef       Path to the lef file
+  --deff      Path to the routed def file
+  --resizer   Path to resizer binary files
+  --clk       Clock period of the design"""
+
+
+import os
+import argparse 
+
+
+
+def read_file(filepath):
+	try:
+		with open(filepath, 'r') as f:
+			return f.readlines()
+	except IOError:
+		 print('Could not read the following file:',filepath)
+	
+		
+def write_file(filepath,line):
+	try:
+		with open(filepath, 'a') as f:
+			f.write(line)
+		f.close()
+	except IOError:
+		print('Could not write in the following file:',filepath)
+
+
+#Parses arguments from the command line 
+
+def ParseArg():
+	parser = argparse.ArgumentParser(description= 'Generates power map')
+	parser.add_argument('--lib',type=str,required=True,metavar='', help='Path to the library file')
+	parser.add_argument('--lef',type=str,required=True, metavar='',help='Path to the lef file')
+	parser.add_argument('--deff',type=str,required=True, metavar='',help='Path to the routed def file')
+	parser.add_argument('--resizer',type=str,required=True, metavar='',help='Path to resizer binary files')
+	parser.add_argument('--clk',type=str,required=True, metavar='',help='Clock period of the design')
+	return parser.parse_args()
+	
+
+
+#Writes a script file to be used by resizer 
+		
+def ScriptTemplate(args):
+	line='read_liberty '
+	line='%s %s %s'%(line,args.lib,'\n')
+	write_file("Script_template",line)
+
+	
+	line='read_lef '
+	line='%s %s %s'%(line,args.lef,'\n')
+	write_file("Script_template",line)
+
+
+	line='read_def '
+	line='%s %s %s'%(line,args.deff,'\n')
+	write_file("Script_template",line)
+
+
+	line='create_clock -name clk -period '
+	line='%s %s %s %s'%(line,args.clk,'{clk}','\n')
+	write_file("Script_template",line)
+
+
+	
+
+#Parses the def file and returns the coordinates and the list of instances, as well as the associated resizer command
+
+def ParseDef(args):
+	inst=[]
+	xcoord=[]
+	ycoord=[]
+	listline=read_file(args.deff)
+	mylines=[]
+	
+	for line in listline:
+		mylines.append(line.split())
+
+	for i in range(len(mylines)):
+		if(len(mylines[i])!=0):
+			if(mylines[i][0]=='COMPONENTS'):
+				break
+
+	start=i+1
+
+	for j in range(start,len(mylines)):
+    	    if(len(mylines[j])!=0):
+        	        if(mylines[j][0]=='END'):
+            	            break
+
+	end=j
+
+
+	for i in range(start,end):
+		if(len(mylines[i])>1):
+			inst.append(mylines[i][1])
+			xcoord.append(mylines[i][mylines[i].index('(')+1])
+			ycoord.append(mylines[i][mylines[i].index('(')+2])
+
+	for i in range(len(inst)):
+		inst[i]=inst[i].replace('\\','')
+
+	command='report_power -instances "'
+
+	for i in range(0,len(inst)):
+		command='%s %s'%(command,inst[i])
+
+	command='%s" > out'%(command)
+	return command, xcoord,ycoord,inst;
+
+
+
+
+# Runs resizer and parses its output to generate the output file that contains the coordinates and the power of each instance
+
+def RunResizer(args,command,xcoord,ycoord,inst):
+	stamylines=[]
+	stainst=[]
+	stapower=[]
+	write_file("Script_template",command)
+	exec_command='%s %s'%(args.resizer,' -exit Script_template')
+	os.system(exec_command)
+	stalistline=read_file('out')
+
+
+	for line in stalistline:
+		stamylines.append(line.split())
+
+	for i in range(3,len(stamylines)):
+		stapower.append(stamylines[i][3])
+		stainst.append(stamylines[i][4])
+
+
+	for i in range(len(stapower)):
+		st=stapower[i]
+		st='%s %s %s \n'%(st,xcoord[inst.index(stainst[i])],ycoord[inst.index(stainst[i])])
+		write_file("outputhotspot",st)
+
+
+if __name__ == "__main__":
+	args=ParseArg()
+	ScriptTemplate(args)
+	command,xcoord,ycoorrd,inst=ParseDef(args)
+	RunResizer(args,command,xcoord,ycoorrd,inst)
+	
+	
+	
+
