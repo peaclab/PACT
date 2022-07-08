526dca417acb8f466ea6091697293cca81121ab5
NevesLucas <neves.lucas.m@gmail.com>
2022-07-08 00:28:43 -0400

    disable parallel container build due to mpi issues

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -l
 
-docker build --pull --rm -f "Docker\parallel.dockerfile" -t pact:latest-parallel .
+#docker build --pull --rm -f "Docker\parallel.dockerfile" -t pact:latest-parallel .   <-- not currently working
+
+###TODO: check if models are already downloaded before building docker image, get them if not
+
 docker build --pull --rm -f "Docker\serial.dockerfile" -t pact:latest-serial .
