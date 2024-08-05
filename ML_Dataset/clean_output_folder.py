aa4ef3a3a600a2502fe3044b3f0fca1e4f7cd7a3
ZhouA25 <134117858+ZhouA25@users.noreply.github.com>
2024-08-05 01:02:41 -0400

    Delete ML_Dataset directory

--- ML_Dataset/clean_output_folder.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-os.chdir('outputs')
-outputs = os.listdir()
-
-for file in outputs:
-    if ".cir.csv" not in file:
-        path = os.getcwd()
-        os.remove(f"{path}/{file}")
